06a03c09db8e3d57e7646d220ecdf94e1647ae05
Iago Viotti <iago.viotti@gmail.com>
2025-02-24 12:40:36 -0300

    :construction: bonusPage in construction, vertical charts done (no logic)

--- src/pages/BonusPage.vue
+++ src/pages/BonusPage.vue
@@ -1,50 +1,62 @@
 <template>
-  <div>
-    <div>
-      <h2>INCOME</h2>
-      <h1>R$ 567,00</h1>
+  <div class="app-finance">
+    <div class="header">
+      <div>
+        <h4>INCOME</h4>
+        <h1>R$ 567,00</h1>
+      </div>
+      <h2>LOGO</h2>
     </div>
-    <h2>LOGO</h2>
-  </div>
-  <navbar>
-    <nav>
+    <div class="navbar">
       <ul>
-        <li><router-link to="/">SEND</router-link></li>
-        <li><router-link to="/bonus">CHANGE</router-link></li>
-        <li><router-link to="/about">PAY</router-link></li>
-        <li><router-link to="/about">BORROW</router-link></li>
-        <li><router-link to="/about">INSURANCE</router-link></li>
+        <label class="nav-label">
+          <li>SEND</li>
+        </label>
+        <label class="nav-label">
+          <li>CHANGE</li>
+        </label>
+        <label class=" nav-label">
+          <li>PAY</li>
+        </label>
+        <label class="nav-label">
+          <li>BORROW</li>
+        </label class="nav-label">
+        <label>
+          <li>INSURANCE</li>
+        </label>
       </ul>
-    </nav>
-  </navbar>
-  <div class="income-dashboard">
-    <h2>INCOME</h2>
-    <h1>R$ 1567,00</h1>
-    <div clas="graph-div">
-      <!-- svg graphs -->
-      <img src="../assets/graph.svg" alt="graph" />
     </div>
-  </div>
-  <div class="expenses-dashboard">
-    <div>
-      <h2>EXPENSES</h2>
-      <h1>R$ 898,00</h1>
+    <div class="income-dashboard">
+      <div class="dashboard-header">
+        <h2>INCOME</h2>
+        <h1>R$ 1567,00</h1>
+      </div>
+      <div clas="graph-div">
+        <lineChart />
+      </div>
     </div>
-    <img src="../assets/arrow-down.svg" alt="graph" />
-  </div>
-  <div v-for="expense in expenses">
-    <div>
-      <h3>{{ expense.name }}</h3>
-      <p>{{ expense.category }}</p>
+    <div class="expenses-dashboard">
+      <div>
+        <h2>EXPENSES</h2>
+        <h1>R$ 898,00</h1>
+      </div>
+      <img src="../assets/arrow-down.svg" alt="graph" />
     </div>
-    <div>
-      <h3>R$ {{ expense.value }}</h3>
+    <div v-for="expense in expenses">
+      <div>
+        <h3>{{ expense.name }}</h3>
+        <p>{{ expense.category }}</p>
+      </div>
+      <div>
+        <h3>R$ {{ expense.value }}</h3>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import lineChart from '../components/lineChart.vue';
 
 const expenses = ref([
   {
@@ -75,3 +87,116 @@ const expenses = ref([
 ]);
 
 </script>
+
+<style lang="less">
+.app-finance {
+  background: rgb(56, 56, 56);
+  background: linear-gradient(0deg, rgba(255, 255, 255, 0) 80%, rgba(119, 108, 230, 1) 88%);
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2em;
+  padding-bottom: 0;
+  color: white;
+}
+
+.balance {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.header h1 {
+  font-size: 2.5em;
+  margin: 0;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  color: white;
+  font-weight: 100;
+}
+
+.header h2 {
+  font-size: 1.5em;
+  margin: 0;
+  align-self: flex-start;
+  color: white;
+}
+
+.header h4 {
+  font-size: 0.9em;
+  margin: 0;
+  align-self: flex-start;
+}
+
+.navbar {
+  display: flex;
+  width: 100%;
+  overflow-x: auto;
+  padding: 15px 15px;
+}
+
+.navbar ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar label {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 20px;
+  height: 70px;
+  width: 70px;
+  font-size: 0.8em;
+  margin: 0 10px;
+}
+
+.navbar li {
+  list-style: none;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+.income-dashboard {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  background-color: white;
+  width: 80%;
+  justify-self: center;
+  align-self: center;
+  border-radius: 20px;
+}
+
+.dashboard-header {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.income-dashboard h2 {
+  font-size: 1.1em;
+  margin: 0;
+  font-weight: 300;
+  color: black;
+}
+
+.income-dashboard h1 {
+  font-size: 2.3em;
+  margin: 0;
+  font-weight: 300;
+  color: black;
+}
+</style>
